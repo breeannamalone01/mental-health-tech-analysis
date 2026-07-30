8100cf731352615d4e9638a9ae87829b051a0400
BreeAnna Malone <breeannamalone@gmail.com>
2026-07-30 09:31:26 -0400

    Rewrite SQL queries against cleaned dataset

--- sql/query2_remote_work.sql
+++ sql/query2_remote_work.sql
@@ -1,9 +1,11 @@
-SELECT 
-  Do_you_work_remotely_,
+SELECT
+  `Do you work remotely_` AS remote_work_status,
   COUNT(*) AS total,
-  COUNTIF(Do_you_currently_have_a_mental_health_disorder_ = 'Yes') AS has_disorder,
-  ROUND((COUNTIF(Do_you_currently_have_a_mental_health_disorder_ = 'Yes')/ COUNT(*) * 100), 2) AS pct_with_disorder
-FROM `true-elevator-380814.mental_health_project.mental_health_in_tech_2016`
-WHERE Do_you_currently_have_a_mental_health_disorder_ IS NOT NULL AND Do_you_work_remotely_ IS NOT NULL
-GROUP BY Do_you_work_remotely_
-ORDER BY pct_with_disorder DESC
+  COUNTIF(`Do you currently have a mental health disorder_` = 'Yes') AS has_disorder,
+  ROUND(COUNTIF(`Do you currently have a mental health disorder_` = 'Yes') / COUNT(*) * 100, 2) AS pct_with_disorder
+FROM
+  `true-elevator-380814.mental_health_project.mental_health_in_tech_2016_cleaned`
+GROUP BY
+  remote_work_status
+ORDER BY
+  pct_with_disorder DESC
--- sql/query4_fear_consequences.sql
+++ sql/query4_fear_consequences.sql
@@ -1,9 +1,11 @@
 SELECT
-  Do_you_think_that_discussing_a_mental_health_disorder_with_your_employer_would_have_negative_consequences_,
+  `Do you think that discussing a mental health disorder with your employer would have negative consequences_` AS fear_of_consequences,
   COUNT(*) AS total,
-  ROUND(((COUNTIF(Have_you_ever_sought_treatment_for_a_mental_health_issue_from_a_mental_health_professional_ = 1)/ COUNT(*))* 100), 2) AS pct_sought_treatment
-FROM `true-elevator-380814.mental_health_project.mental_health_in_tech_2016`
-WHERE Do_you_think_that_discussing_a_mental_health_disorder_with_your_employer_would_have_negative_consequences_ IS NOT NULL
-  AND Have_you_ever_sought_treatment_for_a_mental_health_issue_from_a_mental_health_professional_ IS NOT NULL
-GROUP BY Do_you_think_that_discussing_a_mental_health_disorder_with_your_employer_would_have_negative_consequences_
-ORDER BY pct_sought_treatment DESC
+  COUNTIF(`Have you ever sought treatment for a mental health issue from a mental health professional_` = 1) AS sought_treatment,
+  ROUND(COUNTIF(`Have you ever sought treatment for a mental health issue from a mental health professional_` = 1) / COUNT(*) * 100, 2) AS pct_sought_treatment
+FROM
+  `true-elevator-380814.mental_health_project.mental_health_in_tech_2016_cleaned`
+GROUP BY
+  fear_of_consequences
+ORDER BY
+  pct_sought_treatment DESC
